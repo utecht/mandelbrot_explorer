83243b7017a6ef0095637653500eeabd4b2b8b79
Joseph Utecht <joseph.utecht@gmail.com>
2018-08-23 17:15:20 -0500

    Initial Commit

---
+++ Mandelbrot Explorer macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Mandelbrot Explorer macOS
+//
+//  Created by Joseph Utecht on 8/23/18.
+//  Copyright © 2018 Joseph Utecht. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
